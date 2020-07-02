8a48dededef8df81ea1811cdacd0947dae40fcc3
voilera <voilera@macbookdeagnes>
2020-07-02 17:31:14 +0200

    test2

--- TestJenkins/src/testJenkins/testJenkins.java
+++ TestJenkins/src/testJenkins/testJenkins.java
@@ -5,7 +5,8 @@ public class testJenkins {
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++) {
 			
-			System.out.println("Hello World... "+i);
+			System.out.println("Hello World... ");
+			System.out.println("reccurence"+i);
 		}
 
 	}
