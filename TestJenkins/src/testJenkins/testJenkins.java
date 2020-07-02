c1e6013d43d7db7bd38b8a76621e0e60ef226414
voilera <voilera@macbookdeagnes>
2020-07-02 16:48:59 +0200

    first commit - add project

---
+++ TestJenkins/src/testJenkins/testJenkins.java
@@ -0,0 +1,13 @@
+package testJenkins;
+
+public class testJenkins {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println("Hello World... "+i);
+		}
+
+	}
+
+}
